7a833a10388b3bdaa6cb506d2f69980b5bf9d2d7
Paul Lewallen <paul.lewallen@gmail.com>
2015-01-17 16:05:58 -0800

    code dump

---
+++ chapter2/02-eval-overwrite.js
@@ -0,0 +1,10 @@
+// enabling strict mode for this entire file
+// comment out the line below in order to run the same test w/o strict mode
+"use strict";
+
+// all of the examples below are syntax errors (when in strict mode)
+// comment them out 1-by-1 to test them out
+eval = 1;
+++eval;
+var eval;
+function eval() { }
